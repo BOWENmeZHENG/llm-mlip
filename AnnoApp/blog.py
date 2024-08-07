eff6e1f7703ddcc7fae60003402c9587ae46c512
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2024-08-06 17:11:19 -0700

    typo

--- AnnoApp/blog.py
+++ AnnoApp/blog.py
@@ -10,12 +10,12 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
+    texts = db.execute(
         'SELECT p.id, title, body, annotation, created'
-        ' FROM post p'
+        ' FROM mlip p'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html', texts=texts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
@@ -25,74 +25,74 @@ def create():
         annotation = request.form['annotation']
         db = get_db()
         db.execute(
-            'INSERT INTO post (title, body, annotation)'
+            'INSERT INTO mlip (title, body, annotation)'
             ' VALUES (?, ?, ?)',
             (title, body, annotation)
         )
         db.commit()
-        posts = db.execute(
+        texts = db.execute(
             'SELECT p.id, title, body, annotation, created'
-            ' FROM post p'
+            ' FROM mlip p'
             ' ORDER BY created DESC'
         ).fetchall()
-        return render_template('blog/index.html', posts=posts)
+        return render_template('blog/index.html', texts=texts)
 
     return render_template('blog/create.html')
 
 @bp.route('/<int:id>/annotate', methods=('GET', 'POST'))
 def annotate(id):
-    post = get_post(id)
+    text = get_text(id)
     if request.method == 'GET':
-        text = post['body']
-        annotation = post['annotation'] # it's working
-        word_list = split_para(text)
+        t = text['body']
+        annotation = text['annotation'] # it's working
+        word_list = split_para(t)
         write_anno(f'annotate_{id}', word_list)
         
-        return render_template(f'blog/annotate_{id}.html', post=word_list, annotation_exist=annotation, ID=id)
+        return render_template(f'blog/annotate_{id}.html', text=word_list, annotation_exist=annotation, ID=id)
     
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
+def get_text(id, check_author=True):
+    text = get_db().execute(
         'SELECT p.id, title, body, annotation, created'
-        ' FROM post p'
+        ' FROM mlip p'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
 
-    return post
+    return text
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 def update(id):
-    post = get_post(id)
+    text = get_text(id)
     db = get_db()
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
         annotation = request.form['annotation']
         db.execute(
-            'UPDATE post SET title = ?, body = ?, annotation = ?'
+            'UPDATE mlip SET title = ?, body = ?, annotation = ?'
             ' WHERE id = ?',
             (title, body, annotation, id)
         )
         db.commit()
-        posts = db.execute(
+        texts = db.execute(
             'SELECT p.id, title, body, annotation, created'
-            ' FROM post p'
+            ' FROM mlip p'
             ' ORDER BY created DESC'
         ).fetchall()
-        return render_template('blog/index.html', posts=posts)
+        return render_template('blog/index.html', texts=texts)
 
-    return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', text=text)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM mlip WHERE id = ?', (id,))
     db.commit()
     flash(f"Record has been deleted!")
-    posts = db.execute(
+    texts = db.execute(
         'SELECT p.id, title, body, annotation, created'
-        ' FROM post p'
+        ' FROM mlip p'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html', texts=texts)
